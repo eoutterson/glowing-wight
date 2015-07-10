b96f093cd7faa67627b44727009fa323dc1efabd
EOutterson <eoutterson@gmail.com>
2015-07-10 10:27:48 -0700

    updated with shufflealgorithm

--- tests.js
+++ tests.js
@@ -5,3 +5,18 @@ function twoEggDrop(floors){
   var answerTwo = (-1-Math.sqrt(1-4*(-2*floors)))/2;
   return Math.ceil(Math.max(answerOne,answerTwo));
 }
+
+function fisherYatesShuffle(arr){
+  for (var i = 0; i < arr.length; i++){
+    var randomPosition = Math.floor(Math.random()*(arr.length-i))+i;
+    var temp;
+    temp = arr[i];
+    arr[i] = arr[randomPosition];
+    arr[randomPosition]=temp;
+  }
+  return arr;
+}
+
+function wordCount()
+
+console.log(inPlaceShuffle([1,2,3,4,5]));
