b7ec8875bac714f616de7bafe5d7c028c6909eec
EOutterson <eoutterson@gmail.com>
2015-08-25 14:59:15 -0700

    duplicates in java

--- tests2.java
+++ tests2.java
@@ -469,3 +469,35 @@ public class Q_05 {
     System.out.println(x);
   }
 }
+
+
+
+void deleteDups(LinkedListNode n) {
+  HashSet<Integer> set = new HashSet<Integer>();
+  LinkedListNode previous = null;
+  while (n != null) {
+    if (set.contains(n.data)) {
+      previous.next = n.next;
+    } else {
+      set.add(n.data);
+      previous = n;
+    }
+    n = n.next;
+  }
+}
+
+void deleteDups(LinkedListNode head) {
+  LinkedListNode current = head;
+  while (current != null) {
+    /* Remove all future nodes that have the same value */
+    LinkedListNode runner = current;
+    while (runner.next != null) { 
+      if (runner.next.data == current.data) {
+        runner.next = runner.next.next;
+      } else {
+        runner = runner.next;
+      }
+    }
+    current = current.next;
+  }
+}
